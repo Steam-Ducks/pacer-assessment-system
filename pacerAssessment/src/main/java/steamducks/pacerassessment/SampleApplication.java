7293003a63f6598971aabfdfc22308b8267fc938
Lucas Acosta <lucas.heck@live.com>
2024-09-13 09:22:10 -0300

    🧹 chore: remove unnecessary files

--- pacerAssessment/src/main/java/steamducks/pacerassessment/SampleApplication.java
+++ pacerAssessment/src/main/java/steamducks/pacerassessment/SampleApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class SampleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/telas/steamducks.pacerassessment/menuView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/steamducks.pacerassessment/menuView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Menu foda");
         stage.setScene(scene);
