a4720256d25fdfc4100cb619402ae7c7c4e3327c
Lucas Acosta <70411977+Lucas-heck-acosta@users.noreply.github.com>
2024-09-12 21:47:07 -0300

    remove duplicate directorie

--- pacer-assessment/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pacer.pacerassessment {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens pacer.pacerassessment to javafx.fxml;
-    exports pacer.pacerassessment;
-}
