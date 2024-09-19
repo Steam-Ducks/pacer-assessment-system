de7bb68e29272daa9bf7eb5c27f4416e7ff1a921
arafaellacruz <rafaella.leticiaa@gmail.com>
2024-09-19 14:55:58 -0300

    ✨ feat: adding logo and changing temporary title program

--- pacerAssessment/src/main/java/steamducks/pacerassessment/SampleApplication.java
+++ pacerAssessment/src/main/java/steamducks/pacerassessment/SampleApplication.java
@@ -1,18 +1,21 @@
 package steamducks.pacerassessment;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class SampleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/steamducks.pacerassessment/menuView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Menu foda");
+        stage.setTitle("Pacer Assessment");
+        Image logo = new Image(getClass().getResourceAsStream("/assets/logo-teste.png"));
+        stage.getIcons().add(logo);
         stage.setScene(scene);
         stage.show();
     }
