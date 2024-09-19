13a5d2216d7f1fd7211d1c4a8e858ee02037d3b1
arafaellacruz <rafaella.leticiaa@gmail.com>
2024-09-19 14:56:12 -0300

    ✨ feat: adding new methods and components

--- pacerAssessment/src/main/java/steamducks/pacerassessment/MenuController.java
+++ pacerAssessment/src/main/java/steamducks/pacerassessment/MenuController.java
@@ -1,22 +1,47 @@
 package steamducks.pacerassessment;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
 public class MenuController {
+
+@FXML
+    private Button btnAvaliarSprint;
 
     @FXML
     private Button btnCadastrarAluno;
 
     @FXML
     private Button btnCadastrarSemestre;
+
+    @FXML
+    private Button btnExportar;
+
+    @FXML
+    private Button btnGerenciarCriterios;
+
+    @FXML
+    private Button btnGerenciarEquipes;
+
+    @FXML
+    private Button btnGerenciarSemestre;
+
+    @FXML
+    private ComboBox<?> cmbBoxEquipe;
+
+    @FXML
+    private ComboBox<?> cmbBoxSemestre;
+
+    @FXML
+    private ComboBox<?> cmbBoxSprint;
 
     @FXML
     void abrirCadastroAluno(ActionEvent event) {
@@ -33,12 +58,38 @@ public class MenuController {
 
             Stage stage = new Stage();
             stage.setTitle("Cadastro de Semestre");
+
             stage.setScene(scene);
             stage.show();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void abrirGerenciarCriterios(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirGerenciarEquipes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirGerenciarSemestre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exportar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gerarRelatorio(ActionEvent event) {
+
     }
 
 }
