16f167e890af25f0c0c6208bc70657f6911ddd6d
Samuel Prado <samuelprado.99@hotmail.com>
2024-09-20 23:23:30 -0300

    :goal_net: switch for the login screen to work

--- pacerAssessment/src/main/java/steamducks/pacerassessment/SampleApplication.java
+++ pacerAssessment/src/main/java/steamducks/pacerassessment/SampleApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class SampleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/steamducks.pacerassessment/menuView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/steamducks.pacerassessment/telaLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Menu foda");
+        stage.setTitle("Tela Login");
         stage.setScene(scene);
         stage.show();
     }
