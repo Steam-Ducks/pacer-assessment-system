6a0aa182254e37b0df705b20876037ca93e7c41c
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-09-22 18:14:40 -0300

    :wastebasket:remove:Removing unecessary folder CriteriosMenu

--- CriteriosMenu/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.steamducks.criteriosavaliacao {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens steamducks.pacerassessment to javafx.fxml;
-    exports steamducks.pacerassessment;
-}
