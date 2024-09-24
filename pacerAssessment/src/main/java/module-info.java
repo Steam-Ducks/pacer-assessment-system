05343001cac5ab55aec401a74980b2ab6d8de5e6
Carlos Daniel <carlosdanielalves2006@gmail.com>
2024-09-24 20:17:47 -0300

    :sparkles: feat: adding sprint scoring screen to menu

--- pacerAssessment/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module steamducks.pacerassessment {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens steamducks.pacerassessment to javafx.fxml;
-    exports steamducks.pacerassessment;
-}
