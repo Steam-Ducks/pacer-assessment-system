825849375b1c93e257173e9d5433605ec8ddd7f2
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2024-09-26 20:29:32 -0300

    feat: adding new methods

--- pacerAssessment/src/main/java/steamducks/pacerassessment/MenuProfessorController.java
+++ pacerAssessment/src/main/java/steamducks/pacerassessment/MenuProfessorController.java
@@ -1,6 +1,7 @@
 package steamducks.pacerassessment;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,7 +16,10 @@ import javafx.stage.Stage;
 public class MenuProfessorController {
 
     @FXML
-    private Button btnAvaliarSprint;
+    private Button btnPontuacaoSprint;
+
+    @FXML
+    private Button btnLogout;
 
     @FXML
     private Button btnCadastrarAluno;
@@ -48,7 +52,16 @@ public class MenuProfessorController {
     private ComboBox<?> cmbBoxSprint;
 
     @FXML
-    void abrirAvaliacaoSprint(ActionEvent event) {
+    void abrirLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirCadastroSprint(ActionEvent event) {
+    }
+
+    @FXML
+    void abrirPontuacaoSprint(ActionEvent event) {
 
     }
 
@@ -64,25 +77,7 @@ public class MenuProfessorController {
 
     @FXML
     void abrirCadastroSemestre(ActionEvent event) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/steamducks.pacerassessment/cadastrarSemestreView.fxml"));
-            Parent root = fxmlLoader.load();
-
-            Scene scene = new Scene(root);
-
-            Stage stage = new Stage();
-            stage.setTitle("Cadastro de Semestre");
-
-            Image logo = new Image(getClass().getResourceAsStream("/assets/logo-teste.png"));
-            stage.getIcons().add(logo);
-
-
-            stage.setScene(scene);
-            stage.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        loadView("/steamducks.pacerassessment/cadastrarSemestreView.fxml", "Cadastro de Semestre");
     }
 
     @FXML
@@ -110,4 +105,25 @@ public class MenuProfessorController {
 
     }
 
+    private void loadView(String fxmlFile, String nomeTela) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+
+            Stage stage = new Stage();
+            stage.setTitle(nomeTela);
+
+            Image logo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/assets/logo-teste.png")));
+            stage.getIcons().add(logo);
+
+
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
