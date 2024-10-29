5ec898e77dfcc1723dd75d1bab5f78f89368d602
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2024-10-29 01:31:17 -0300

    refactor: adding id attribute

--- pacerAssessment/src/main/java/steamducks/pacerassessment/models/Semestre.java
+++ pacerAssessment/src/main/java/steamducks/pacerassessment/models/Semestre.java
@@ -2,11 +2,20 @@ package steamducks.pacerassessment.models;
 
 public class Semestre {
 
+    private int id;
     private String nome;
 
 
-    public Semestre(String nome) {
+    public Semestre(int id, String nome) {
         this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
